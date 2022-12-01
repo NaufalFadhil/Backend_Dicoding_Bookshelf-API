ada31cfc4e624492a1373cb47a953e8dd8696c3d
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-12-01 21:44:39 +0700

    feat: create add book handler and add cors

---
+++ src/handler.js
@@ -0,0 +1,60 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+exports.addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+
+  const updatedAt = new Date().toISOString();
+  const createdAt = updatedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    createdAt,
+    updatedAt,
+  };
+
+  console.log(newBook);
+
+  books.push(newBook);
+
+  console.log(books);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
