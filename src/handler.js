68de1de0cea1908324dff3285920a0e7bf80d315
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-12-02 11:07:02 +0700

    feat: create delete book

--- src/handler.js
+++ src/handler.js
@@ -108,11 +108,37 @@ exports.editBookByIdHandler = (request, h) => {
     response.code(200);
     return response;
   }
- 
+
   const response = h.response({
     status: 'fail',
     message: 'Gagal memperbarui catatan. Id tidak ditemukan',
   });
+  response.code(404);
+  return response;
+};
+
+exports.deleteBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+
   response.code(404);
   return response;
 };
